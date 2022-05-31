52a46c77716edf1ffe2b6d8757ef7f6028d4b7f2
Farhan Hasan <hasanfarhan33@gmail.com>
2022-05-31 14:32:31 +0530

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,26 @@ public class Main {
         return maxValue;
     }
 
+    public static int smallSubArray(int[] input, int targetSum) {
+        int minWindowSize = Integer.MAX_VALUE;
+        int windowStart = 0;
+        int currentWindowSum = 0;
+        for(int windowEnd = 0; windowEnd < input.length; windowEnd++) {
+            currentWindowSum += input[windowEnd];
+
+            while(currentWindowSum >= targetSum) {
+                minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
+                currentWindowSum -= input[windowStart];
+                windowStart+=1;
+            }
+        }
+        return minWindowSize;
+    }
+
     public static void main(String[] args) {
-        System.out.println(findMaxSubarray(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3)); 
+        System.out.println(findMaxSubarray(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3));
+        int targetSum = 8;
+        int[] input = new int[] {4, 2, 2, 7, 8, 1, 2, 8, 1, 0};
+        System.out.println(smallSubArray(input, targetSum));
     }
 }
