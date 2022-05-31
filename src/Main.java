0e2a863cbf45c86af62152c4285b149986ca279b
Farhan Hasan <hasanfarhan33@gmail.com>
2022-05-31 14:01:00 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static int findMaxSubarray(int[] arr, int k) {
+        int maxValue = Integer.MIN_VALUE;
+        int currentRunningSum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            currentRunningSum += arr[i];
+            if(i >= k-1) {
+                maxValue = Math.max(maxValue, currentRunningSum);
+                currentRunningSum -= arr[i - (k - 1)];
+            }
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMaxSubarray(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3)); 
+    }
+}
